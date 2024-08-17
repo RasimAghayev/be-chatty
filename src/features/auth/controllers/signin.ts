86a9afa42e5f68aef61b962aa5cdb625591b9437
Rasim Aghayev <rasimaqayev@gmail.com>
2024-08-17 09:00:25 +0400

    72. Test email with password reset templates

--- src/features/auth/controllers/signin.ts
+++ src/features/auth/controllers/signin.ts
@@ -7,8 +7,13 @@ import { authService } from '@service/db/auth.service';
 import { BadRequestError } from '@global/helpers/error-handler';
 import { loginSchema } from '@auth/schemes/signin';
 import { IAuthDocument } from '@auth/interfaces/auth.interface';
-import { IUserDocument } from '@user/interfaces/user.interface';
+import { IResetPasswordParams, IUserDocument } from '@user/interfaces/user.interface';
 import { userService } from '@service/db/user.service';
+import { emailQueue } from '@service/queues/email.queue';
+import publicIP from 'ip';
+import dayjs from 'dayjs';
+import { forgotPasswordTemplate } from '@service/emails/templates/forgot-password/forgot-password-template';
+import { resetPasswordTemplate } from '@service/emails/templates/reset-password/reset-password-template';
 
 export class SignIn {
   @joiValidation(loginSchema)
@@ -35,6 +40,26 @@ export class SignIn {
       },
       config.JWT_TOKEN!
     );
+    const templateParams: IResetPasswordParams = {
+      username: existingUser.username!,
+      email: existingUser.email!,
+      ipaddress: publicIP.address(),
+      date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+    };
+    const resetLink = `${config.CLIENT_URL}/reset-password?token=${userJwt}`;
+    const template1: string = forgotPasswordTemplate.forgotPasswordConfirmationTemplate(existingUser.username!, resetLink);
+    const template2: string = resetPasswordTemplate.passwordResetConfirmationTemplate(templateParams);
+    emailQueue.addEmailJob('forgotPasswordEmail', {
+      template: template1,
+      receiverEmail: 'daphney19@ethereal.email',
+      subject: 'forgotPasswordEmailTemplate',
+    });
+    emailQueue.addEmailJob('forgotPasswordEmail', {
+      template: template2,
+      receiverEmail: 'daphney19@ethereal.email',
+      subject: 'passwordResetConfirmationTemplate',
+    });
+
     req.session = { jwt: userJwt };
     const userDocument: IUserDocument = {
       ...user,
@@ -45,6 +70,7 @@ export class SignIn {
       uId: existingUser!.uId,
       createdAt: existingUser!.createdAt,
     } as IUserDocument;
+
     res.status(HTTP_STATUS.OK).json({
       message: 'User login successfully.',
       user: userDocument,
