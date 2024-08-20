03b1bce1b4f6b4003610d0af197d0cda02598754
Rasim Aghayev <rasimaqayev@gmail.com>
2024-08-20 21:00:23 +0400

    fix: 68. Mail transport class

--- src/shared/workers/email.worker.ts
+++ src/shared/workers/email.worker.ts
@@ -1,7 +1,7 @@
 import { DoneCallback, Job } from 'bull';
 import Logger from 'bunyan';
 import { config } from '@root/config';
-import { mailTransport } from '@service/emails/mail.transports';
+import { mailTransport } from '@service/emails/mail.transport';
 
 const log: Logger = config.createLogger('emailWorker');
 
